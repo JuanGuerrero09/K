911e8e4d7964ce6719f62f89ea35e46b4753c50c
juanguerrero09 <juanguerrero09mc@gmail.com>
2024-07-03 21:06:59 +0200

    Add parser and move ui functions

--- main.go
+++ main.go
@@ -1,169 +1,93 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/joho/godotenv"
-	// "github.com/charmbracelet/huh"
+	"strings"
+	"time"
 )
 
-type Styles struct {
-	BorderColor lipgloss.Color
-	InputField  lipgloss.Style
-}
-
-type Question struct {
-	question string
-	answer   string
-	input    Input
-}
-
-func NewQuestion(question string) Question {
-	return Question{question: question}
-}
-
-func NewShortQuestion(question string) Question {
-	q := NewQuestion(question)
-	field := NewShortAnswerField()
-	q.input = field
-	return q
-}
-
-func NewLongQuestion(question string) Question {
-	q := NewQuestion(question)
-	field := NewLongAnswerField()
-	q.input = field
-	return q
-}
-
-func DefaultStyles() *Styles {
-	s := new(Styles)
-	s.BorderColor = lipgloss.Color("36")
-	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
-	return s
-}
-
-type Main struct {
-	index     int
-	questions []Question
-	height    int
-	width     int
-	styles    *Styles
-	done      bool
-}
-
-func NewMain(questions []Question) *Main {
-	styles := DefaultStyles()
-	answerField := textinput.New()
-	answerField.Focus()
-	answerField.Placeholder = "Your answer here: "
-	return &Main{questions: questions, styles: styles}
-}
-
-func (m Main) Init() tea.Cmd {
-	return nil
-}
-
-func (m Main) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	current := &m.questions[m.index]
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.height = msg.Height
-		m.width = msg.Width
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			if m.index == len(m.questions)-1 {
-				m.done = true
-			}
-			current.answer = current.input.Value()
-			log.Printf("question: %s, answer: %s", current.question, current.answer)
-			m.Next()
-			return m, current.input.Blur
-		}
-	}
-	current.input, cmd = current.input.Update(msg)
-	return m, cmd
-}
-
-func (m *Main) Next() {
-	if m.index < len(m.questions)-1 {
-		m.index++
-	} else {
-		m.index = 0
-	}
-}
-
-func (m Main) View() string {
-	if m.done {
-		var output string
-		for _, q := range m.questions {
-			output += fmt.Sprintf("%s, %s\n", q.question, q.answer)
-		}
-		return output
-	}
-	if m.width == 0 {
-		return "loading..."
-	}
-	currentQuestion := m.questions[m.index]
-	return lipgloss.Place(
-		m.width,
-		m.height,
-		lipgloss.Center,
-		lipgloss.Center,
-		lipgloss.JoinVertical(
-			lipgloss.Center,
-			currentQuestion.question,
-			m.styles.InputField.Render(currentQuestion.input.View())),
-	)
+type ToDo struct {
+	isDone         bool
+	Text           string
+	Category       string
+	CompletionDate time.Time
 }
 
 func main() {
-
-	err := godotenv.Load()
+	file, err := os.Open("list.txt")
 	if err != nil {
-    log.Fatal("Error loading .env file")
-  }
-
-	name := os.Getenv("NAME")
-	partnerName := os.Getenv("PARTNERNAME")
-
-	log.Printf("The name is: %s", name)
-	log.Printf("The partnername is: %s", partnerName)
-
-	questions := []Question{NewShortQuestion("What's your name?"), NewShortQuestion("What's your partner name?"), NewLongQuestion("What do you love about him?")}
-	m := NewMain(questions)
-
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		fmt.Println("fatal: ", err)
-		os.Exit(0)
+		fmt.Println("Error opening file:", err)
+		return
 	}
-	defer f.Close()
+	defer file.Close()
 
-	p := tea.NewProgram(m, tea.WithAltScreen())
+	todos, err := ParseFile(file)
+	if err != nil {
+		fmt.Println("Error parsing file:", err)
+		return
+	}
 
-	p.Run()
+	for _, todo := range todos {
+		fmt.Printf("%+v\n", todo)
+	}
+}
 
-	// var name string
-	// var name2 string
-	// form := huh.NewForm(
-	// 	huh.NewGroup(huh.NewInput().Description("What should we call you?").Value(&name), huh.NewInput().Description("What should we call you?").Value(&name2)),
-	// )
+func ParseFile(file *os.File) ([]ToDo, error) {
+	var todos []ToDo
+	var currentCategory string
+	startParsing := false
 
-	// err := form.Run()
-	// if err != nil {
-	// 	fmt.Println("error:", err)
-	// }
+	scanner := bufio.NewScanner(file)
 
-	// fmt.Println("Welcome, " + name + "!")
-	// fmt.Println("Welcome, " + name2 + "!")
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if !startParsing {
+			if strings.HasPrefix(line, "###") {
+				startParsing = true
+			}
+			continue
+		}
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(line, "@") {
+			currentCategory = strings.TrimPrefix(line, "@")
+		} else if strings.HasPrefix(line, "<E>") {
+			currentCategory = "Bonus España"
+			} else if strings.HasPrefix(line, "<EU>") {
+			currentCategory = "Bonus Europa"
+			}else if strings.HasPrefix(line, "-") {
+			todo := ToDo{
+				isDone:   false,
+				Text:     strings.TrimSpace(strings.TrimPrefix(line, "-")),
+				Category: currentCategory,
+			}
+			todos = append(todos, todo)
+		} else if strings.HasPrefix(line, "F") {
+			parts := strings.Split(line, "/")
+			if len(parts) > 2 {
+				return nil, fmt.Errorf("invalid format for completed task: %s", line)
+			}
+			dateStr := strings.TrimSpace(parts[1])
+			completationDate, err := time.Parse("2006-01-02", dateStr)
+			if err != nil {
+				return nil, fmt.Errorf("invalid date format: %s", dateStr)
+			}
+			todo := ToDo{
+				isDone:         true,
+				Text:           strings.TrimSpace(strings.TrimPrefix("F", parts[0])),
+				Category:       currentCategory,
+				CompletionDate: completationDate,
+			}
+			todos = append(todos, todo)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return todos, nil
+
 }
---
+++ ui.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/joho/godotenv"
+	// "github.com/charmbracelet/huh"
+)
+
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+type Question struct {
+	question string
+	answer   string
+	input    Input
+}
+
+func NewQuestion(question string) Question {
+	return Question{question: question}
+}
+
+func NewShortQuestion(question string) Question {
+	q := NewQuestion(question)
+	field := NewShortAnswerField()
+	q.input = field
+	return q
+}
+
+func NewLongQuestion(question string) Question {
+	q := NewQuestion(question)
+	field := NewLongAnswerField()
+	q.input = field
+	return q
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+	return s
+}
+
+type Main struct {
+	index     int
+	questions []Question
+	height    int
+	width     int
+	styles    *Styles
+	done      bool
+}
+
+func NewMain(questions []Question) *Main {
+	styles := DefaultStyles()
+	answerField := textinput.New()
+	answerField.Focus()
+	answerField.Placeholder = "Your answer here: "
+	return &Main{questions: questions, styles: styles}
+}
+
+func (m Main) Init() tea.Cmd {
+	return nil
+}
+
+func (m Main) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	current := &m.questions[m.index]
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			if m.index == len(m.questions)-1 {
+				m.done = true
+			}
+			current.answer = current.input.Value()
+			log.Printf("question: %s, answer: %s", current.question, current.answer)
+			m.Next()
+			return m, current.input.Blur
+		}
+	}
+	current.input, cmd = current.input.Update(msg)
+	return m, cmd
+}
+
+func (m *Main) Next() {
+	if m.index < len(m.questions)-1 {
+		m.index++
+	} else {
+		m.index = 0
+	}
+}
+
+func (m Main) View() string {
+	if m.done {
+		var output string
+		for _, q := range m.questions {
+			output += fmt.Sprintf("%s, %s\n", q.question, q.answer)
+		}
+		return output
+	}
+	if m.width == 0 {
+		return "loading..."
+	}
+	currentQuestion := m.questions[m.index]
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.JoinVertical(
+			lipgloss.Center,
+			currentQuestion.question,
+			m.styles.InputField.Render(currentQuestion.input.View())),
+	)
+}
+
+func teaui() {
+
+	err := godotenv.Load()
+	if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+	name := os.Getenv("NAME")
+	partnerName := os.Getenv("PARTNERNAME")
+
+	log.Printf("The name is: %s", name)
+	log.Printf("The partnername is: %s", partnerName)
+
+	questions := []Question{NewShortQuestion("What's your name?"), NewShortQuestion("What's your partner name?"), NewLongQuestion("What do you love about him?")}
+	m := NewMain(questions)
+
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Println("fatal: ", err)
+		os.Exit(0)
+	}
+	defer f.Close()
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	p.Run()
+
+	// var name string
+	// var name2 string
+	// form := huh.NewForm(
+	// 	huh.NewGroup(huh.NewInput().Description("What should we call you?").Value(&name), huh.NewInput().Description("What should we call you?").Value(&name2)),
+	// )
+
+	// err := form.Run()
+	// if err != nil {
+	// 	fmt.Println("error:", err)
+	// }
+
+	// fmt.Println("Welcome, " + name + "!")
+	// fmt.Println("Welcome, " + name2 + "!")
+}
