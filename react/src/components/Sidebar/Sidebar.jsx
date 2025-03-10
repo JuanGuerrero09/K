fb84433d052b10d4cd5d647ffee25ef9e1deac25
juanguerrero09 <juanguerrero09mc@gmail.com>
2025-03-10 23:59:28 +0100

    added progress bar in the sidebar

--- react/src/components/Sidebar/Sidebar.jsx
+++ react/src/components/Sidebar/Sidebar.jsx
@@ -6,14 +6,36 @@ export default function Sidebar() {
         aria-label="close sidebar"
         className="drawer-overlay"
       ></label>
-      <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+      <ul className="menu flex bg-base-100 text-base-content min-h-full w-80 p-4">
         {/* Sidebar content here */}
+        {/* For TSX uncomment the commented types below */}
+        <h2 className="text-3xl mt-4 self-center font-bold text-blue-600">
+          Progress
+        </h2>
+        <div
+          className="radial-progress self-center mt-6"
+          style={{ "--value": 70 } /* as React.CSSProperties */}
+          aria-valuenow={70}
+          role="progressbar"
+        >
+          70%
+        </div>
         <li>
           <a>Sidebar Item 1</a>
         </li>
+        <progress
+          className="progress progress-primary w-56"
+          value={50}
+          max="100"
+        ></progress>
         <li>
           <a>Sidebar Item 2</a>
         </li>
+        <progress
+          className="progress progress-primary w-56"
+          value="70"
+          max="100"
+        ></progress>
       </ul>
     </>
   );
